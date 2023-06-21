d5274f2290c3ae17371f0c055086435bfbee96ac
Rinat <rinat-yar@mail.ru>
2023-06-22 00:16:37 +0300

    Initial commit

---
+++ src/test/java/ru/netology/test/CashbackHackTest.java
@@ -0,0 +1,94 @@
+package ru.netology.test;
+
+import ru.netology.CashBackHack;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class CashbackHackTest {
+
+    @org.testng.annotations.Test
+    public void testPurchase() {
+
+        CashBackHack service = new CashBackHack();
+
+        int amount = 1500;
+        int expected = 500;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testPurchase_1() {
+
+        CashBackHack service = new CashBackHack();
+
+        int amount = 1;
+        int expected = 999;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testPurchase_2() {
+
+        CashBackHack service = new CashBackHack();
+
+        int amount = 0;
+        int expected = 1000;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testPurchase_3() {
+
+        CashBackHack service = new CashBackHack();
+
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testPurchase_4() {
+
+        CashBackHack service = new CashBackHack();
+
+        int amount = 999;
+        int expected = 1;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+
+    }
+
+    @org.testng.annotations.Test
+    public void testPurchase_5() {
+
+        CashBackHack service = new CashBackHack();
+
+        int amount = 1001;
+        int expected = 999;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testPurchase_6() {
+
+        CashBackHack service = new CashBackHack();
+
+        int amount = 2000;
+        int expected = 0;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+}
+
